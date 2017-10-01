2706a18651165270bbf35d0972859aca88314b1e
ddkaty <ddkaty@gmail.com>
2017-10-01 23:17:06 +0800

    修改README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # exapp
 
+cd /Users/apple/keys/vsproject/exapp
+1. git init
+2. VScode 操作：暂存当前目录的改变 #git add README.md
+3. VScode 操作：提交改变 #git commit -m "first commit"
+4. git remote add origin https://github.com/harryluo163/C-Tests.git
+5. git push -u origin master
---
+++ public/blog/mail-notiflier.js
@@ -0,0 +1,37 @@
+const notifier = require('mail-notifier');
+const fs = require('fs');
+const pinyin = require("pinyin");
+const unzip2 = require("unzip2");
+
+
+const imap = {
+  user: "linklong1000@163.com",
+  password: "szkdzz4321",
+  host: "imap.163.com",
+  port: 993, // imap port
+  tls: true,// use secure connection
+  tlsOptions: { rejectUnauthorized: false }
+};
+
+const n = notifier(imap);
+n.on('end', () => n.start()) // session closed
+  .on('mail', function(mail) {
+        mail.attachments.forEach( function(item) {
+                console.log(item);
+                //console.log(mail.from[0].address, mail.subject);
+                name = pinyin(item.fileName.split('.')[0],{
+                        style: pinyin.STYLE_FIRST_LETTER
+                }).join('')
+                fs.writeFileSync("./"+name+".zip",item.content);
+                fs.createReadStream("./"+name+".zip")
+                  .pipe(unzip2.Extract({ path: "./"}))
+                  .on('close', function () {
+                        fs.unlink("./"+name+".zip")
+                        fs.rename(item.fileName.split('.')[0],"html/"+name,function(err){
+                                if(err)
+                                console.log(err)
+                        });
+                  });
+        });
+  })
+  .start();
